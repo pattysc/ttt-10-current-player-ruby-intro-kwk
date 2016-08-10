b9ac2ef5f3417ba3262701b8efdf4135c6a1317c
p sc <patty.stacruz@gmail.com>
2016-08-10 21:59:09 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 def turn_count(board)
-  counter = 0
+  counter = 1
   board.each do |value|
     if value == "O" || value == "X"
       counter += 1
@@ -9,7 +9,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 != 0
+  if turn_count(board).even?
     return "O"
   else
     return "X"
